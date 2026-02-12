c2b6857fd43b1802a592d94f0d3aa1999b69d796
Micheal00110 <mikejamal254@gmail.com>
2026-02-12 12:28:43 +0300

    chore: upgrade next to patched version to fix CVE-2025-55182

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
